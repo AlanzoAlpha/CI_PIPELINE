d8820b14f1baeaa33e34b38edcb119be8c7f499c
Ubuntu <ubuntu@ip-172-31-45-201.eu-west-1.compute.internal>
2020-11-21 15:40:05 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+
+cd ~/cne-sfia2-brief/terraform
+terraform init
+terraform plan
+terraform apply -auto-approve
+
+export ip="$(terraform output ec2_ip)"
+
+sleep 30
+
+cd ~/cne-sfia2-brief/ansible
+ansible-playbook -i ${ip}, playbook.yaml
+
+sleep 60
+ssh StrictHostKeyChecking accept-new ubuntu@${ip}
+
